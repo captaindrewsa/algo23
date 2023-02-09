d03ce5780aa0e8e54a12c208a142ec3a8458e5fe
Andrey Vlasov <captaindrewsa@gmail.com>
2023-02-09 14:27:06 +0200

    Try to create a simple recursion

--- lucky-tickets/src/lib.rs
+++ lucky-tickets/src/lib.rs
@@ -51,3 +51,44 @@ pub fn simple_solver_for_ten() -> i32 {
 
     return count;
 }
+
+pub fn test_recursion(number_in_row: i32) -> i32 {
+    struct FibonaciCond {
+        value_now: i32,     //5 8
+        value_before1: i32, //3 5
+        value_before2: i32, //2 3
+        ordinal_number: i32,
+    }
+
+    impl Default for FibonaciCond {
+        fn default() -> Self {
+            Self {
+                value_now: 1,
+                value_before1: 1,
+                value_before2: 0,
+                ordinal_number: 3,
+            }
+        }
+    }
+
+    impl FibonaciCond {
+        fn calculate(&self) -> Self {
+            let res = self.value_before1 + self.value_before2;
+            Self {
+                value_now: res,
+                value_before1: self.value_now,
+                value_before2: self.value_before1,
+                ordinal_number: self.ordinal_number + 1,
+            }
+        }
+    }
+
+    let mut stack_of_task: Vec<FibonaciCond> = Vec::new();
+
+    while stack_of_task.len() > 0 {
+        let task = stack_of_task.pop().unwrap();
+        if task.ordinal_number == number_in_row {}
+    }
+
+    return 0;
+}
