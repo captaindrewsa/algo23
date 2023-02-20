d7678f4fe18161322b4c75fccb446de95fc67609
Andrey Vlasov <captaindrewsa@gmail.com>
2023-02-20 17:26:16 +0200

    update base-struct

--- base-struct/src/lib.rs
+++ base-struct/src/lib.rs
@@ -1,14 +1,46 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt::Display;
+
+pub struct SingleArray<T> {
+    array: [T;0]
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<T> SingleArray<T>{
+    pub fn new()-> Self{
+        SingleArray{
+            array: [],
+        }
     }
+
+    fn is_empty(&mut self)-> bool{
+        if self.array.len()==0{
+            true
+        } else {
+            false
+        }
+    }
+
+    fn add_one_field_and_copy(&mut self){
+    }
+}
+
+impl<T> IArray<T> for SingleArray<T>{
+    fn add(&mut self, item: T, idx: usize) {
+        if self.is_empty(){
+        }
+    }
+
+    fn remove(&mut self, idx: i32) -> T {
+        todo!()
+    }
+}
+
+pub struct VectorArray {}
+
+pub struct FactorArray {}
+
+pub struct MatrixArray {}
+
+pub trait IArray<T> {
+    fn add(&mut self, item: T, idx: usize);
+    fn remove(&mut self, idx: i32) -> T;
 }
---
+++ base-struct/src/main.rs
@@ -0,0 +1,8 @@
+use base_struct::*;
+
+
+
+fn main() {
+    let mut tmp: SingleArray<i32> = SingleArray::new();
+    tmp.add(132, 10);
+}
