1b0ef73f8db0945ada1130f8dbf0fa3b5ff82c74
Andrey Vlasov <captaindrewsa@gmail.com>
2023-03-02 10:01:37 +0200

    Refractoring filesystem

--- base-struct/src/main.rs
+++
@@ -1,8 +0,0 @@
-use base_struct::*;
-
-
-
-fn main() {
-    let mut tmp: SingleArray<i32> = SingleArray::new();
-    tmp.add(132, 10);
-}
--- lucky-tickets/src/main.rs
+++
@@ -1,6 +0,0 @@
-use lucky_tickets::{simple_solver_for_six, simple_solver_for_ten};
-
-fn main() {
-    println!("{} билетов", simple_solver_for_six());
-    // println!("{} билетов", simple_solver_for_ten());
-}
