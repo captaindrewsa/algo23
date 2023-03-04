624b8b05475975d1a8deabe49b2090b8afb7c03c
Andrey Vlasov <captaindrewsa@gmail.com>
2023-03-04 20:38:29 +0200

    Lucky-tickets in ready. But for max six

---
+++ lucky-tickets/lucky-tickets.py
@@ -0,0 +1,22 @@
+
+def count_of_lucky_ticket(ticket_lenght):
+    max_count_of_sum = (ticket_lenght//2)*9 +1
+    # print(max_count_of_sum)
+    count_of_sums = [0 for _ in range(max_count_of_sum)]
+    for elem in range(int("9"*(ticket_lenght//2))+1):
+        hundreds = elem//100
+        dozens = elem%100//10
+        units = elem%10
+
+        summ = hundreds + dozens + units
+        count_of_sums[summ]+=1
+    
+    result_count = sum([elem*elem for elem in count_of_sums])
+
+    return result_count
+
+
+
+
+
+print("Count of lucky tickets: ", count_of_lucky_ticket(6))
