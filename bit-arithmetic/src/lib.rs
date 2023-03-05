7441b7213429d55659900376ed3c55571b0cfddc
Andrey Vlasov <captaindrewsa@gmail.com>
2023-03-05 15:55:03 +0200

    Something wrong with knight_move

--- bit-arithmetic/src/lib.rs
+++ bit-arithmetic/src/lib.rs
@@ -2,8 +2,11 @@ fn king_move(pos: u32) -> u64{
 
     let king_position = 1u64 << pos;
     let king_no_a : u64 = 0x101010101010101 & king_position;
-    let king_no_h: u64 = 0x8080808080808080 & king_position;
+    let king_no_h : u64 = 0x8080808080808080 & king_position;
     
+    // println!("{:#?}", vec![king_no_a,king_no_h]);
+
+
     let move_mask = 
     (king_no_a>>7)|(king_position>>8)|(king_no_h>>9)|
     (king_no_a<<1)         |          (king_no_h>>1)|
@@ -23,13 +26,21 @@ pub fn king_issue(pos:u32){
     println!("{number_of_move}")
 }
 
-fn knight_move(pos: u32)-> u64{
+pub fn knight_move(pos: u32)-> u64{
 
     let knight_position = 1u64 << pos;
 
-    
+    let knight_no_a: u64 = 0xfefefefefefefefe & knight_position;
+    let knight_no_b: u64 = 0xfdfdfdfdfdfdfdfd & knight_position;
+    let knight_no_g: u64 = 0xbfbfbfbfbfbfbfbf & knight_position;
+    let knight_no_h: u64 = 0x7f7f7f7f7f7f7f7f & knight_position;
+
+    println!("{:#?}", vec![knight_no_a,knight_no_b,knight_no_g,knight_no_h]);
+
     let move_mask = 
-    (knight_position>>6)|(knight_position>>15)|(knight_position>>17)|(knight_position>>10)|
-    (knight_position<<6)|(knight_position<<15)|(knight_position<<17)|(knight_position<<10);
+                    (knight_no_a>>15)|(knight_no_h>>17)|
+    (knight_no_b>>6)                 |                 (knight_no_g>>10)|               
+    (knight_no_b<<10)                |                 (knight_no_g<<6)|
+                    (knight_no_a<<17)|(knight_no_h<<15);
     return move_mask;
 }
--- bit-arithmetic/src/main.rs
+++ bit-arithmetic/src/main.rs
@@ -1,8 +1,8 @@
-use bit_arithmetic::king_issue;
+use bit_arithmetic::{king_issue, knight_move};
 
 
 
 fn main() {
-    // king_issue(23);
-    
+    // king_issue(56);
+    println!("{}", knight_move(38));
 }
