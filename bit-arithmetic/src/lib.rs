862212795de31bfa5a3c982eb7b394b66dae773b
Andrey Vlasov <captaindrewsa@gmail.com>
2023-03-05 19:46:42 +0200

    King_issue resolved

--- bit-arithmetic/src/lib.rs
+++ bit-arithmetic/src/lib.rs
@@ -1,19 +1,19 @@
-fn king_move(pos: u32) -> u64{
+fn king_move(pos: u64) -> u64{
 
     let king_position = 1u64 << pos;
-    let king_no_a : u64 = 0x101010101010101 & king_position;
-    let king_no_h : u64 = 0x8080808080808080 & king_position;
+    let king_no_a : u64 = 0xfefefefefefefefe & king_position;
+    let king_no_h : u64 = 0x7f7f7f7f7f7f7f7f & king_position;
     
     // println!("{:#?}", vec![king_no_a,king_no_h]);
 
 
     let move_mask = 
-    (king_no_a>>7)|(king_position>>8)|(king_no_h>>9)|
-    (king_no_a<<1)         |          (king_no_h>>1)|
-    (king_no_a<<9)|(king_position<<8)|(king_no_h<<7);
+    (king_no_a<<7)|(king_position<<8)|(king_no_h<<9)|
+    (king_no_a>>1)         |          (king_no_h<<1)|
+    (king_no_a>>9)|(king_position>>8)|(king_no_h>>7);
     return move_mask;
 }
-pub fn king_issue(pos:u32){
+pub fn king_issue(pos:u64){
     let mut mask = king_move(pos);
     let mut number_of_move=0;
     while mask>0 {
@@ -26,20 +26,33 @@ pub fn king_issue(pos:u32){
     println!("{number_of_move}")
 }
 
-pub fn knight_move(pos: u32)-> u64{
+pub fn knight_move(pos: u64)-> u64{
 
     let knight_position = 1u64 << pos;
 
-    let knight_no_a: u64 = 0xfefefefefefefefe & knight_position;
-    let knight_no_b: u64 = 0xfdfdfdfdfdfdfdfd & knight_position;
-    let knight_no_g: u64 = 0xbfbfbfbfbfbfbfbf & knight_position;
-    let knight_no_h: u64 = 0x7f7f7f7f7f7f7f7f & knight_position;
-
-
-    let move_mask = 
-                    (knight_no_a<<15)|(knight_no_h<<17)|
-    (knight_no_b<<6)                 |                 (knight_no_g<<10)|               
-    (knight_no_b>>10)                |                 (knight_no_g>>6)|
-                    (knight_no_a>>17)|(knight_no_h>>15);
+    let knight_no_a: u64 = knight_position & 0xfefefefefefefefe;
+    let knight_no_b: u64 = knight_position & 0xfdfdfdfdfdfdfdfd;
+    let knight_no_g: u64 = knight_position & 0xbfbfbfbfbfbfbfbf;
+    let knight_no_h: u64 = knight_position & 0x7f7f7f7f7f7f7f7f;
+    
+    let move_mask =
+    (knight_no_a<<15)|(knight_no_a>>17)|
+    ((knight_no_b&knight_no_a)<<6)|((knight_no_b&knight_no_a)>>10)|
+    ((knight_no_g&knight_no_h)<<10)|((knight_no_g&knight_no_h)>>6)|
+    (knight_no_h<<17)|(knight_no_h>>15);
+    
+    
     return move_mask;
+}
+pub fn knight_issue(pos: u64){
+    let mut mask = knight_move(pos);
+    let mut number_of_move=0;
+    while mask>0 {
+        number_of_move+=mask&1;
+        mask = mask>>1;
+    }
+
+
+    println!("{}", knight_move(pos));
+    println!("{number_of_move}")
 }
--- bit-arithmetic/src/main.rs
+++ bit-arithmetic/src/main.rs
@@ -1,8 +1,8 @@
-use bit_arithmetic::{king_issue, knight_move};
+use bit_arithmetic::{knight_issue, knight_move, king_issue};
 
 
 
 fn main() {
-    // king_issue(56);
-    println!("{}", knight_move(49));
+    // king_issue(32);
+    // knight_issue(6);
 }
