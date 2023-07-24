7911e4515141153720908b081b61272cc8d30957
Andrey Vlasov <captaindrewsa@gmail.com>
2023-07-24 13:24:12 +0200

    Restore on new Computer

--- hashfunction-and-hashmap/src/main.rs
+++ hashfunction-and-hashmap/src/main.rs
@@ -1,40 +1,46 @@
-use std::{collections::LinkedList};
+use std::{collections::VecDeque};
+#[derive(Default)]
 struct KeyValueStorage{
-    size_storage: i32,
-    storage: Vec<Vec<(String, i32)>>
+    size_storage: u32,
+    storage: Vec<VecDeque<(String, i32)>>
 }
 
-impl Default for KeyValueStorage{
-    fn default() -> Self {
-        let mut tmp: Vec<Vec<(String, i32)>> = Vec::new();
+// impl Default for KeyValueStorage{
+//     fn default() -> Self {
+//         let mut tmp: Vec<VecDeque<(String, i32)>> = Vec::new();
+//         let size_storage = 10u32;
 
-        Self{ size_storage: todo!(), storage: todo!() }
-    }
-}
+//         Self{ size_storage: size_storage, storage:  }
+//     }
+// }
 
 impl KeyValueStorage{
     
-    pub fn get(&mut self){}
+    pub fn get(&mut self){
+        
+    }
+    
     pub fn add(&mut self, key: &str, value: i32){
         let key_hash = self.get_hash_code(key);
         let position = self.hash(key_hash, self.storage.len() as u32);
+
 
 
     }
     pub fn repl(&mut self){}
     pub fn del(&mut self){}
 
-    fn get_hash_code(&self, string: &str)-> u32{
+    fn get_hash_code(&self, string: &str)-> usize{
 
         let mut sum = 0;
         for char in string.chars(){
-            sum+= char as u32;
+            sum+= char as usize;
         }
         
         return sum
     }
     
-    fn hash(&mut self, value: u32, lengh_storage: u32)-> u32{
+    fn hash(&mut self, value: usize, lengh_storage: usize)-> usize{
         return value % lengh_storage 
     }
 
@@ -44,10 +50,6 @@ impl KeyValueStorage{
 
 
 fn main() {
-    let mut a: LinkedList<i32> = LinkedList::new();
-    a.push_back(255);
-    a.push_back(32);
-    a.push_back(212312);
 
     println!("");
 }
